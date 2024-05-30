775bfb73734594965d587ab77d57f1c5e0f558ab
Chris Liu <liumchr@amazon.com>
2024-05-30 12:09:34 -0700

    Personalize repo

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -181,5 +181,5 @@ if [ "$KSU" ] && [ -d "/data/adb/modules/zygisk-assistant" ]; then
 fi
 
 ui_print "* Done"
-ui_print "  by j-hc (github.com/j-hc)"
+ui_print "  by xChickens (github.com/xChickens)"
 ui_print " "
